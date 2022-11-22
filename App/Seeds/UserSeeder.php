2a54faa6e16963fc5d5ec2f4065766784af1a198
Caio Albuquerque <caio.albuquerque@infobase.com.br>
2022-11-22 17:33:35 -0300

    WIP

---
+++ App/Seeds/UserSeeder.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Seeds;
+
+use App\Factories\UserFactory;
+use Core\Abstractions\Seeder;
+
+class UserSeeder extends Seeder
+{
+    public static function handler(): void
+    {
+        self::factory('users', UserFactory::class, 10);
+    }
+}
